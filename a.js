0d1b32accff7f40598321874e0c39feee73e218d
vivek <krvivi28@gmail.com>
2023-11-25 20:47:21 +0530

    initial commit

--- a.js
+++
@@ -1,6 +0,0 @@
-// Function to test if a string matches the pattern
-const containsPattern = (inputString) => {
-  return pattern.test(inputString);
-};
-let link = "http://51.20.43.8:5000";
-console.log(containsPattern(link));
